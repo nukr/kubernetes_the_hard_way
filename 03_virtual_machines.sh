d338e4e78d6afc1c10c9e499b2f30b821ee1ab78
Nukr <nukrs.w@gmail.com>
2017-02-13 13:32:00 +0800

    create vm in parallel

--- 03_virtual_machines.sh
+++ 03_virtual_machines.sh
@@ -9,7 +9,7 @@ do
    --image-project ubuntu-os-cloud \
    --machine-type n1-standard-1 \
    --private-network-ip 10.240.0.1$i \
-   --subnet kubernetes
+   --subnet kubernetes&
 done
 
 for i in `seq 0 2`
@@ -21,7 +21,7 @@ do
    --image-project ubuntu-os-cloud \
    --machine-type n1-standard-1 \
    --private-network-ip 10.240.0.2$i \
-   --subnet kubernetes
+   --subnet kubernetes&
 done
 
 for i in `seq 0 2`
@@ -33,5 +33,9 @@ do
    --image-project ubuntu-os-cloud \
    --machine-type n1-standard-1 \
    --private-network-ip 10.240.0.3$i \
-   --subnet kubernetes
+   --subnet kubernetes&
 done
+
+wait
+
+echo "done"
